6ee0d1089fee61faab591e6ed0d6e4cf62df391b
JoseTomanan <jdtomanan@up.edu.ph>
2025-10-08 11:47:57 +0800

    feat: Use gemini way of processing images; Add logic for get_response

--- crude/main.py
+++ crude/main.py
@@ -1,34 +1,48 @@
 """
-Crude application for evaluating answers using AI.
+Crude application for evaluating answers using AI. Powered by Google Gemini API.
 """
-import google.generativeai as genai
+import os
+
+from google import genai
+from google.genai import types
 
 from base64 import b64encode
+from dotenv import load_dotenv
 from PIL import Image
 
 
 class AIAnswerEvaluator:
 	def __init__(self):
-		api_key = ...
-
-		self.model = "gemini-1.5-pro"
-
-		genai.configure(api_key=api_key)
-		self.client = genai.GenerativeModel(self.model)
-
-	def encode_image(self, image_path):
-		"""Encode image in base64"""
+		api_key = os.getenv("GEMINI_API_KEY")
+		self.client = genai.Client(api_key=api_key)
+		
+	def get_image(self, image_path: str) -> bytes:
+		"""Get unencoded image"""
 		with open(image_path, "rb") as image_file:
-			encoded_file = b64encode(image_file.read())
-			return encoded_file.decode("utf-8")
+			return image_file.read()
 
-	def get_response(self, image_path, system_prompt, user_prompt):
-		"""Send a chat completion request with the image input"""
-		...
+	def get_response(self, image_path: str, system_prompt: str, user_prompt: str):
+		"""Send a chat completion request with the image input. Uses inline image for now"""
+		image_bytes = self.get_image(image_path)
+		
+		image_encoded = types.Part.from_bytes(
+				data=image_bytes,
+				mime_type='image_jpeg'
+			)
+
+		response = self.client.models.generate_content(
+				model="gemini-2.5-pro",
+				contents=[
+					image_encoded,
+					f"{system_prompt} {user_prompt}"
+				]
+			)
+
+		return response.text
 
 
 if __name__ == "__main__":
-	image_path = "../dataset/2.jpeg"
+	load_dotenv()
 
 	system_prompt = """
 		You are given an image of a student's handwritten work in response to a math problem.
@@ -38,8 +52,10 @@ if __name__ == "__main__":
 		For example, given question "what is the equation shown in the image?", generate your answer as: "3x+2=8".
 		"""
 	
-	expected_answer = ""
-	user_prompt = f"Is the final answer the same as '{expected_answer}'?"
+	image_path = "../dataset/2.jpeg"
+	expected_answer = "2"
+	
+	user_prompt = f"Can the final answer (boxed) be interpreted as '{expected_answer}'?"
 	
 	ai_evaluator = AIAnswerEvaluator()
 	response = ai_evaluator.get_response(image_path, system_prompt, user_prompt)
