f418eb4de8d81ffbe3950675ffc7bba9cdf0aaf3
HoweZae <jdtomanan@up.edu.ph>
2025-10-08 13:39:00 +0800

    chore: segregate prompts to file

--- crude/all_prompts.py
+++ crude/all_prompts.py
@@ -2,6 +2,13 @@
 All prompts used for evaluating answers.
 """
 
-GENERATE_ANSWER_PROMPT = ""
+ANSWER_RUBRIC_PROMPT: str = """
+	You are given an image of a student's handwritten work in response to a math problem.
+	The student's work is shown on the right side of the image, and the problem is displayed on the left side for context.
+	Your task is to answer a question based solely on the visual content of the student's work, present on the right side of the image.
+	Your answer should be clear and concise, and directly relate to the image presented on the right side of the given image. 
+	If the question can be answered with a yes or no question, only generate your answer as "YES" or "NO".
+	Otherwise, for example, given question "what is the equation shown in the image?", generate your answer as "3x+2=8".
+	"""
 
-EVALUATE_ANSWER_PROMPT = ""
+GENERATE_OWN_ANSWER_PROMPT: str = ""
--- crude/main.py
+++ crude/main.py
@@ -3,11 +3,15 @@ Crude application for evaluating answers using AI. Powered by Google Gemini API.
 """
 import os
 
+from all_prompts import ANSWER_RUBRIC_PROMPT
+
 from google import genai
 from google.genai import types
-
 from dotenv import load_dotenv
 
+
+class ImagePreprocessor:
+	...
 
 class AIAnswerEvaluator:
 	def __init__(self):
@@ -22,7 +26,6 @@ class AIAnswerEvaluator:
 	def get_response(self, image_path: str, system_prompt: str, user_prompt: str):
 		"""Send a chat completion request with the image input"""
 		image_bytes = self.get_image(image_path)
-		
 		image_encoded = types.Part.from_bytes(
 				data=image_bytes,
 				mime_type='image/jpeg'
@@ -42,13 +45,7 @@ class AIAnswerEvaluator:
 if __name__ == "__main__":
 	load_dotenv()
 
-	system_prompt = """
-		You are given an image of a student's handwritten work in response to a math problem.
-		The student's work is shown on the right side of the image, and the problem is displayed on the left side for context.
-		Your task is to answer a question based solely on the visual content of the student's work, present on the right side of the image.
-		Your answer should be clear and concise, and directly relate to the image presented on the right side of the given image. 
-		For example, given question "what is the equation shown in the image?", generate your answer as: "3x+2=8".
-		"""
+	system_prompt = ANSWER_RUBRIC_PROMPT
 	
 	image_path = "dataset/2.jpeg"
 	expected_answer = "2"
