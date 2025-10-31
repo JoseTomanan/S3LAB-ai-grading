5388af7baa23c5a579e92e8a1d67e629858e805f
HoweZae <jdtomanan@up.edu.ph>
2025-10-31 20:25:26 +0800

    chore: remove warnings

--- scripts/extractor_to_txt.py
+++ scripts/extractor_to_txt.py
@@ -13,7 +13,7 @@ def extract_csv_column_to_txt(csv_path: str, column_name: str, output_path: str)
 	# Read the CSV file
 	with open(csv_path, mode="r", encoding="utf-8", newline="") as csvfile:
 		reader = csv.DictReader(csvfile)
-		if column_name not in reader.fieldnames:
+		if column_name not in reader.fieldnames: # pyright: ignore[reportOperatorIssue]
 			raise ValueError(f"Column '{column_name}' not found in CSV headers: {reader.fieldnames}")
 	
 		for row in reader:
