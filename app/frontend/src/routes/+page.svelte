f637ad36856097a6db29eb9ae465055a1cfc72f9
HoweZae <jdtomanan@up.edu.ph>
2025-11-20 12:56:33 +0800

    feat: add sample data; add sample renderer (to add: TestInstance component)

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import type { TestInstance } from '$lib/types';
+	
+	let instances: TestInstance[] = [
+		{test_name: "Quiz 1", section: "3-Aguinaldo", date: "kasbu erp", is_done_rendering: true},
+		{test_name: "Quiz 2", section: "3-Aguinaldo", date: "next week erp", is_done_rendering: false},
+	];
+</script>
+
+<h1>View test instances</h1>
+
+<div class="flex flex-col gap-6">
+	{#each instances as instance}
+		<pre>{JSON.stringify(instance, null, 2)}</pre>
+	{/each}
+</div>
