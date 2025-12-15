4ed9ed4021f032483a3105f6acfaee538af0b9b2
JoseTomanan <jdtomanan@up.edu.ph>
2025-12-15 12:31:21 +0800

    feat: add IS_LINED variable

--- scripts/CamScanner-In-Python/Scanner.py
+++ scripts/CamScanner-In-Python/Scanner.py
@@ -3,35 +3,38 @@ import numpy as np
 import mapper
 
 
+IS_LINED = False
+
 
 if __name__ == "__main__":
-    image = cv2.imread("dataset/contour_13.jpg")	# read in the image
+    image = cv2.imread("ignoreable/white4croppedB.jpg")	# read in the image
     assert image is not None
 
-    image=cv2.resize(image,(1300,800)) 	# resizing because opencv does not work well with bigger images
+    image=cv2.resize(image,(800,1300)) 	# resizing because opencv does not work well with bigger images
     orig=image.copy()
 
 	# STEP 1 : MAKE GRAYSCALE (TEMPORARILY)
     gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-    # STEP 2: CREATE HORIZONTAL KERNEL (for morphological operations)
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (400, 1))
+    if IS_LINED:
+        # STEP 2: CREATE HORIZONTAL KERNEL (for morphological operations)
+        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (400, 1))
 
-    # STEP 3: DETECT HORIZONTAL LINES
-    detected_lines = cv2.morphologyEx(gray, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-    cv2.imshow("Detected lines", detected_lines)
+        # STEP 3: DETECT HORIZONTAL LINES
+        detected_lines = cv2.morphologyEx(gray, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+        cv2.imshow("Detected lines", detected_lines)
 
-    # STEP 4: SUBTRACT LINES FROM ORIGINAL
-    gray_no_lines = cv2.subtract(gray, detected_lines)
-    cv2.imshow("Without Lines", gray_no_lines)
-    
-    # STEP 4.1: ADAPTIVE THRESHOLDING
-    thresh = cv2.adaptiveThreshold(gray_no_lines, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                   cv2.THRESH_BINARY, 3, 2)
-    cv2.imshow("Adaptive threshold", thresh)
+        # STEP 4: SUBTRACT LINES FROM ORIGINAL
+        gray_no_lines = cv2.subtract(gray, detected_lines)
+        cv2.imshow("Without Lines", gray_no_lines)
+        
+        # STEP 4.1: ADAPTIVE THRESHOLDING
+        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                       cv2.THRESH_BINARY, 3, 2)
+        cv2.imshow("Adaptive threshold", thresh)
 
     # STEP 5: APPLY GAUSSIAN BLUR
-    blurred = cv2.GaussianBlur(gray_no_lines, (5,5), 0)  # (5,5) is the kernel size and 0 is sigma that determines the amount of blur
+    blurred = cv2.GaussianBlur(thresh if IS_LINED else gray, (5,5), 0)  # (5,5) is the kernel size and 0 is sigma that determines the amount of blur
     cv2.imshow("Blur",blurred)
 
     # STEP 6: SEGMENT CANNY LINES
