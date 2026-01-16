47b0815b273cf08a4edc25ad99eb4ea072f22d64
HoweZae <jdtomanan@up.edu.ph>
2026-01-16 17:41:49 +0800

    feat: move link to outer div; add card to end of name

--- app/frontend/src/lib/components/TestInstanceCard.svelte
+++ app/frontend/src/lib/components/TestInstanceCard.svelte
@@ -2,17 +2,19 @@
 	export let test_name: string;
 	export let section: string;
 	export let date: string;
-	export let is_done_rendering: string;
+	export let is_done_rendering: boolean;
+
+	const testid: string = test_name + section;
 </script>
 
-<div class="rounded card">
+<div class="rounded card border border-border p-1">
 	{#if is_done_rendering}
 		<!-- TODO: add done rendering logo -->
 	{:else}
 		<!-- TODO: add not done rendering logo -->
 	{/if}
-	<div>
-		<h3><a href="_blank">{test_name}</a></h3>
+	<a href={testid}>
+		<h3>{test_name}</h3>
 		<h4>{section} || {date}</h4>
-	</div>
+	</a>
 </div>
