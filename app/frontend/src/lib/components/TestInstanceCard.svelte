b15fbb6870802902e4e929b8715c1e723c0038a3
HoweZae <jdtomanan@up.edu.ph>
2026-01-16 18:25:40 +0800

    feat: parse date object

--- app/frontend/src/lib/components/TestInstanceCard.svelte
+++ app/frontend/src/lib/components/TestInstanceCard.svelte
@@ -4,17 +4,22 @@
 	export let date: string;
 	export let is_done_rendering: boolean;
 
+	import Icon from '@iconify/svelte';
+
 	const testid: string = test_name + section;
+	const dateObj: Date = new Date(date);
 </script>
 
-<div class="rounded card border border-border p-1">
-	{#if is_done_rendering}
-		<!-- TODO: add done rendering logo -->
-	{:else}
-		<!-- TODO: add not done rendering logo -->
-	{/if}
+<div class="flex gap-2 rounded card border border-border px-3 py-1.5">
 	<a href={testid}>
-		<h3>{test_name}</h3>
-		<h4>{section} || {date}</h4>
+		<h3 class="flex flex-row items-center gap-1">
+			{#if is_done_rendering}
+				<Icon icon="proicons:checkbox-checked" class="size-6"/>
+			{:else}
+				<Icon icon="proicons:checkbox-unchecked" class="size-6"/>
+			{/if}
+			{test_name}
+		</h3>
+		<h4>{section} &centerdot; {dateObj.toLocaleDateString()}</h4>
 	</a>
 </div>
