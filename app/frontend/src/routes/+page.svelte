d7cf04794f17c46620c05a4184ecfed9f5f17164
HoweZae <jdtomanan@up.edu.ph>
2026-01-16 18:26:07 +0800

    feat: spacing

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -3,16 +3,17 @@
 	import TestInstanceCard from '$lib/components/TestInstanceCard.svelte';
 	
 	let instances: TestInstance[] = [
-		{test_name: "Quiz 0", section: "3-Rizal", date: "2025-11-11T20:17:46.384Z", is_done_rendering: true},
+		{test_name: "Seatwork 1", section: "3-Rizal", date: "2025-11-11T20:17:46.384Z", is_done_rendering: true},
+		{test_name: "Seatwork 2", section: "3-Aguinaldo", date: "2025-12-12T20:17:46.384Z", is_done_rendering: false},
 		{test_name: "Quiz 1", section: "3-Aguinaldo", date: "2026-01-12T20:17:46.384Z", is_done_rendering: true},
-		{test_name: "Quiz 2", section: "3-Aguinaldo", date: "2025-12-12T20:17:46.384Z", is_done_rendering: false},
 	];
 </script>
 
-<h1>View Test Instances</h1>
-
-<div class="flex flex-col gap-2">
-	{#each instances as instance}
-		<TestInstanceCard {...instance} />
-	{/each}
+<div class="flex flex-col gap-4">
+	<h1>View Test Instances</h1>
+	<div class="flex flex-col gap-3">
+		{#each instances as instance}
+			<TestInstanceCard {...instance} />
+		{/each}
+	</div>
 </div>
