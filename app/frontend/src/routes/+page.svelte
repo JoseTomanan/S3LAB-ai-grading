3c7166650773ff0530fd9dd5ebef5680b34d1165
HoweZae <jdtomanan@up.edu.ph>
2026-01-16 17:42:06 +0800

    feat: pass instance props

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
 	import type { TestInstance } from '$lib/types.ts';
+	import TestInstanceCard from '$lib/components/TestInstanceCard.svelte';
 	
 	let instances: TestInstance[] = [
+		{test_name: "Quiz 0", section: "3-Rizal", date: "2025-11-11T20:17:46.384Z", is_done_rendering: true},
 		{test_name: "Quiz 1", section: "3-Aguinaldo", date: "2026-01-12T20:17:46.384Z", is_done_rendering: true},
 		{test_name: "Quiz 2", section: "3-Aguinaldo", date: "2025-12-12T20:17:46.384Z", is_done_rendering: false},
-	];
-
-	let done_instances: TestInstance[] = [
-		{test_name: "Quiz 0", section: "3-Rizal", date: "2025-11-11T20:17:46.384Z", is_done_rendering: true},
 	];
 </script>
 
 <h1>View Test Instances</h1>
 
-<div class="flex flex-col gap-6">
+<div class="flex flex-col gap-2">
 	{#each instances as instance}
-		<pre>{JSON.stringify(instance, null, 2)}</pre>
-	{/each}
-	<hr>
-	{#each done_instances as done_instance}
-		<pre class="opacity-80">
-			{JSON.stringify(done_instance, null, 2)}
-		</pre>
+		<TestInstanceCard {...instance} />
 	{/each}
 </div>
