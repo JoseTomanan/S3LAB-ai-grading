ad7594c02306ad2e6641d8ff6a71348e735a7350
HoweZae <jdtomanan@up.edu.ph>
2026-01-17 16:23:11 +0800

    feat: add buttons with icons; Add variables for count of short form and prob sol questions; Use icon pack

--- app/frontend/src/routes/[testid]/+page.svelte
+++ app/frontend/src/routes/[testid]/+page.svelte
@@ -3,33 +3,38 @@
 	// export let section: string;
 	// export let date: string;
 	// export let is_done_rendering: boolean;
-	
-	import Icon from '@iconify/svelte';
+
+	import MdiArrowBack from '~icons/mdi/arrow-back';
+	import MdiTable from '~icons/mdi/table';
+	import MdiUpload from '~icons/mdi/upload';
 
 	$: test_name = "Seatwork 1";
 	$: section = "3-Rizal";
-	$: string = "";
+	$: date = "2025-11-11T20:17:46.384Z";
 	$: is_done_rendering = false;
+
+	$: countShortFormItems = 0;
+	$: countProbSolItems = 0;
 
 </script>
 
 <div class="px-2 py-4">
 	<div class="bg-sidebar p-4 border-b border-sidebar-border space-y-2">
 		<span class="flex flex-row items-center gap-2">
-			<span class="size-8">
-				<Icon icon="proicons:arrow-left" class="size-full"/>
-			</span>
+			<a class="size-8" href=".">
+				<MdiArrowBack class="size-full" />
+			</a>
 			<h1>{test_name}</h1>
 		</span>
 		<div id="change-instance-details">
 			<h3>Section: {section}</h3>
-			<h3>No. of Short-form: ...</h3>
-			<h3>No. of Problem solving: ...</h3>
+			<h3>No. of Short-form: {countShortFormItems}</h3>
+			<h3>No. of Problem solving: {countProbSolItems}</h3>
 		</div>
-		<span>
-			<!-- TODO : open class list button -->
-			<!-- TODO : open spreadsheet button -->
-			<!-- TODO : batch upload button -->
+		<span id="thisOne" class="flex items-center gap-2 justify-end">
+			<button>Open class list</button>
+			<button><MdiTable class="size-6"/></button>
+			<button><MdiUpload class="size-6"/></button>
 		</span>
 	</div>
 	<div>
