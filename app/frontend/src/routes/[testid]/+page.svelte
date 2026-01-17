ee5533211bd2ad111476d56dffc63a23a28ef045
HoweZae <jdtomanan@up.edu.ph>
2026-01-17 12:23:18 +0800

    feat: make variables reactive; add enclosing div to details; etc

--- app/frontend/src/routes/[testid]/+page.svelte
+++ app/frontend/src/routes/[testid]/+page.svelte
@@ -3,24 +3,34 @@
 	// export let section: string;
 	// export let date: string;
 	// export let is_done_rendering: boolean;
-
-	const test_name: string = "Seatwork 1";
-	const section: string = "3-Rizal";
-	const date: string = "";
-	const is_done_rendering: boolean = false;
-
+	
 	import Icon from '@iconify/svelte';
+
+	$: test_name = "Seatwork 1";
+	$: section = "3-Rizal";
+	$: string = "";
+	$: is_done_rendering = false;
+
 </script>
 
 <div class="px-2 py-4">
-	<div class="bg-secondary p-4 border-b border-border">
+	<div class="bg-sidebar p-4 border-b border-sidebar-border space-y-2">
 		<span class="flex flex-row items-center gap-2">
-			<Icon icon="proicons:arrow-left" class="size-8"/>
+			<span class="size-8">
+				<Icon icon="proicons:arrow-left" class="size-full"/>
+			</span>
 			<h1>{test_name}</h1>
 		</span>
-		<h3>Section: {section}</h3>
-		<h3>No. of Short-form: ...</h3>
-		<h3>No. of Problem Solving: ...</h3>
+		<div id="change-instance-details">
+			<h3>Section: {section}</h3>
+			<h3>No. of Short-form: ...</h3>
+			<h3>No. of Problem solving: ...</h3>
+		</div>
+		<span>
+			<!-- TODO : open class list button -->
+			<!-- TODO : open spreadsheet button -->
+			<!-- TODO : batch upload button -->
+		</span>
 	</div>
 	<div>
 	
