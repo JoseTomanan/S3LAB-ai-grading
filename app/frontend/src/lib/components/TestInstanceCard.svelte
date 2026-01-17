5f46e73a3e801c4be2b5c85de11ab6540db6467a
HoweZae <jdtomanan@up.edu.ph>
2026-01-17 16:10:59 +0800

    fix: replace with MDI icon pack

--- app/frontend/src/lib/components/TestInstanceCard.svelte
+++ app/frontend/src/lib/components/TestInstanceCard.svelte
@@ -4,7 +4,8 @@
 	export let date: string;
 	export let is_done_rendering: boolean;
 
-	import Icon from '@iconify/svelte';
+	import MdiCheckboxBlank from '~icons/mdi/checkbox-blank';
+	import MdiCheckboxMarked from '~icons/mdi/checkbox-marked';
 
 	const testid: string = section + "-" + test_name;
 	const dateObj: Date = new Date(date);
@@ -12,8 +13,11 @@
 
 <a href={testid} class="flex flex-col rounded card bg-card px-3 py-1.5">
 	<span class="flex flex-row items-center gap-1">
-		<Icon icon={is_done_rendering ? 'proicons:checkbox-checked' : 'proicons:checkbox-unchecked'}
-				class="size-6"/>
+		{#if is_done_rendering}
+			<MdiCheckboxMarked/>
+		{:else}
+			<MdiCheckboxBlank/>
+		{/if}
 		<h3>{test_name}</h3>
 	</span>
 	<h4>{section} &centerdot; {dateObj.toLocaleDateString()}</h4>
