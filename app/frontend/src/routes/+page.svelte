657a0078097e71260914ce3d8ae3b64abc4c798e
HoweZae <jdtomanan@up.edu.ph>
2026-01-17 11:33:51 +0800

    feat: minor

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -3,9 +3,9 @@
 	import TestInstanceCard from '$lib/components/TestInstanceCard.svelte';
 	
 	let instances: TestInstance[] = [
-		{test_name: "Seatwork 1", section: "3-Rizal", date: "2025-11-11T20:17:46.384Z", is_done_rendering: true},
-		{test_name: "Seatwork 2", section: "3-Aguinaldo", date: "2025-12-12T20:17:46.384Z", is_done_rendering: false},
-		{test_name: "Quiz 1", section: "3-Aguinaldo", date: "2026-01-12T20:17:46.384Z", is_done_rendering: true},
+		{test_name: "Seatwork-1", section: "3-Rizal", date: "2025-11-11T20:17:46.384Z", is_done_rendering: true},
+		{test_name: "Seatwork-2", section: "3-Aguinaldo", date: "2025-12-12T20:17:46.384Z", is_done_rendering: false},
+		{test_name: "Quiz-1", section: "3-Aguinaldo", date: "2026-01-12T20:17:46.384Z", is_done_rendering: true},
 	];
 </script>
 
