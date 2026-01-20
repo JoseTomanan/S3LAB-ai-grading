e28022a5790dd204ee61adb7d3ce1d2db937410c
HoweZae <jdtomanan@up.edu.ph>
2026-01-20 15:48:12 +0800

    refactor: move test topbar to own component

---
+++ app/frontend/src/lib/components/TestDetailsTopbar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let test_name: string;
+	export let section: string;
+	export let date: string;
+	// svelte-ignore export_let_unused
+	export let is_done_rendering: boolean;
+
+	export let countShortFormItems: number;
+	export let countProbSolItems: number;
+
+	import MdiArrowBack from '~icons/mdi/arrow-back';
+	import MdiTable from '~icons/mdi/table';
+	import MdiUpload from '~icons/mdi/upload';
+
+	const dateObj: Date = new Date(date);
+</script>
+
+<div class="bg-sidebar p-4 border-b border-sidebar-border space-y-4">
+	<span class="flex flex-row items-center gap-2">
+		<a class="size-8" href=".">
+			<MdiArrowBack class="size-full" />
+		</a>
+		<h1>{ test_name }</h1>
+	</span>
+	<div id="change-instance-details">
+		<h3>Section: { section }</h3>
+		<h3>Date: { dateObj.toLocaleDateString() }</h3>
+		<h3>No. of Short form: { countShortFormItems }</h3>
+		<h3>No. of Problem solving: { countProbSolItems }</h3>
+	</div>
+	<span id="thisOne" class="flex items-center gap-2 justify-end">
+		<button class="button-primary">Open class list</button>
+		<button class="button-primary">
+			<MdiTable class="size-6" />
+		</button>
+		<button class="button-primary">
+			<MdiUpload class="size-6"/>
+		</button>
+	</span>
+</div>
