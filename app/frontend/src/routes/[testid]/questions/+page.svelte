786b190324153dc60e1403fe72db453f0e4d6311
HoweZae <jdtomanan@up.edu.ph>
2026-01-25 19:10:06 +0800

    feat: add temp values

--- app/frontend/src/routes/[testid]/questions/+page.svelte
+++ app/frontend/src/routes/[testid]/questions/+page.svelte
@@ -4,60 +4,45 @@
 	export let date: string;
 	export let is_done_rendering: boolean;
 
-	import MdiArrowBack from '~icons/mdi/arrow-back';
 	import MdiEdit from '~icons/mdi/edit';
-	import MdiTable from '~icons/mdi/table';
-	import MdiUpload from '~icons/mdi/upload';
 
-	import type { TestInstance, TestQuestion } from '$lib/types/types.ts';
+	import type { TestQuestion } from '$lib/types/types.ts';
+	import TestDetails from '$lib/components/TestInstanceTopbar.svelte';
 
-	$: test_name = "Seatwork 1";
-	$: section = "3-Rizal";
-	$: date = "2025-11-11T20:17:46.384Z";
-	$: is_done_rendering = false;
+	// temporary!
+	test_name = "Seatwork 1";
+	section = "3-Rizal";
+	date = "2025-11-11T20:17:46.384Z";
+	is_done_rendering = false;
 	
-	let shortFormItems: TestQuestion[];
-	let probSolItems: TestQuestion[];
-
-	$: shortFormItems = [];
-	$: probSolItems = [];
+	let allItems: TestQuestion[] = [];
+	let shortFormItems: String[];
+	let probSolItems: String[];
 
 	function retrieveItems() {
-		// TODO: function that retrieves from API call and populates respective arrays
+		// TODO: function that retrieves from API call and populate allQuestions
 	}
 
-	// TODO: SIMPLIFY SHORTFORMITEMS TYPE SHIT
+	// temporary
+	allItems = [];
+
+	$: shortFormItems = allItems.filter(item => !item.is_problem_solving).map(item => item.question);
+	$: probSolItems = allItems.filter(item => item.is_problem_solving).map(item => item.question);
 
 </script>
 
 <div class="py-4 space-y-8">
-	<div class="bg-sidebar p-4 border-b border-sidebar-border space-y-4">
-		<span class="flex flex-row items-center gap-2">
-			<a class="size-8" href="/">
-				<MdiArrowBack class="size-full" />
-			</a>
-			<h1>{ test_name }</h1>
-		</span>
-		<div id="change-instance-details">
-			<h3>Section: { section }</h3>
-			<h3>No. of Short form: { shortFormItems.length }</h3>
-			<h3>No. of Problem solving: { probSolItems.length }</h3>
-		</div>
-		<span id="thisOne" class="flex items-center gap-2 justify-end">
-			<button class="button-primary">Open class list</button>
-			<button class="button-primary"><MdiTable class="size-6"/></button>
-			<button class="button-primary"><MdiUpload class="size-6"/></button>
-		</span>
-	</div>
+	<TestDetails {test_name} {section} {date} {is_done_rendering}
+				countShortFormItems={shortFormItems.length} countProbSolItems={probSolItems.length}/>
 	<div class="px-4 space-y-4">
 		{#each [{name: "Short form Items", items: shortFormItems}, {name: "Problem solving Items", items: probSolItems}] as listOfItems}
-			<div class="p-2 border border-border bg-card">
+			<div class="p-2 bg-card ring">
 				<span class="flex flex-row items-center w-full justify-between">
 					<h3>{ listOfItems.name }</h3>
 					<MdiEdit/>
 				</span>
 				{#each listOfItems.items as item}
-					{ item.question }
+					{ item }
 				{/each}
 			</div>
 		{/each}
