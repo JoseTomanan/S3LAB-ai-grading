725f963a303ed07e0c9b099fc626262accf328fe
JoseTomanan <jdtomanan@up.edu.ph>
2026-01-25 11:52:15 +0800

    feat: change icons to secondaryu theme

--- app/frontend/src/lib/components/TestInstanceCard.svelte
+++ app/frontend/src/lib/components/TestInstanceCard.svelte
@@ -4,19 +4,20 @@
 	export let date: string;
 	export let is_done_rendering: boolean;
 
-	import MdiCheckboxBlank from '~icons/mdi/checkbox-blank';
-	import MdiCheckboxMarked from '~icons/mdi/checkbox-marked';
+	import MdiCheckboxBlankOutline from '~icons/mdi/checkbox-blank-outline';
+	import MdiCheckboxMarkedOutline from '~icons/mdi/checkbox-marked-outline';
 
 	const testid: string = section + "-" + test_name;
 	const dateObj: Date = new Date(date);
 </script>
 
-<a href={testid + "/questions"} class="flex flex-col rounded card bg-card px-3 py-1.5">
-	<span class="flex flex-row items-center gap-1">
+<a href={`/${testid}/questions`}
+			class="flex flex-col rounded card bg-card px-4 py-2">
+	<span class="flex flex-row items-center gap-2">
 		{#if is_done_rendering}
-			<MdiCheckboxMarked/>
+			<MdiCheckboxMarkedOutline/>
 		{:else}
-			<MdiCheckboxBlank/>
+			<MdiCheckboxBlankOutline/>
 		{/if}
 		<h3>{test_name}</h3>
 	</span>
