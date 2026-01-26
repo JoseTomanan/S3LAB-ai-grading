b39d126d564c91c417ebe047577f3ee3656022ee
HoweZae <jdtomanan@up.edu.ph>
2026-01-26 15:01:43 +0800

    refactor: remove fluff

--- crude/_main.py
+++ crude/_main.py
@@ -6,6 +6,7 @@ from classes import *
 from dotenv import load_dotenv
 
 
+SYSTEM_PROMPT = ANSWER_RUBRIC_PROMPT
 IMAGE_PATH = "dataset/contour_15.jpg"
 QUESTION_PATH = "dataset/2.csv"
 RUBRIC_QUESTION = "Based only on the visual content of the student's work, what is the student's final answer? What is the correct mathematical answer to the problem? Are they the same?"
@@ -15,27 +16,18 @@ RUBRIC_QUESTION = "Based only on the visual content of the student's work, what 
 if __name__ == "__main__":
 	load_dotenv()
 
-	image_preprocessor = CVImagePreprocessor()
-	contexter = CSVProcessor()
-	ai_evaluator = AIAnswerEvaluator()
-
-	system_prompt = ANSWER_RUBRIC_PROMPT
-	rubric_question = RUBRIC_QUESTION
-	image_path = IMAGE_PATH
-	question_path = QUESTION_PATH
-
-	context = contexter.get_context(question_path)
-	user_prompt = f"QUESTION: {context[0]}\nPROMPT: {rubric_question}"
+	context = CSVProcessor().get_context(QUESTION_PATH)
+	user_prompt = f"QUESTION: {context[0]}\nPROMPT: {RUBRIC_QUESTION}"
 
 	print(user_prompt)
 	
-	image_bytes = image_preprocessor.load_image(image_path)
-	image_bytes = image_preprocessor.find_first_box(image_bytes)
-	image_bytes = image_preprocessor.brighten(image_bytes, amount=0.2)
-	image_bytes = image_preprocessor.adjust_contrast(image_bytes, amount=1.2)
+	image_bytes = CVImagePreprocessor().load_image(IMAGE_PATH)
+	image_bytes = CVImagePreprocessor().find_first_box(image_bytes)
+	image_bytes = CVImagePreprocessor().brighten(image_bytes, amount=0.2)
+	image_bytes = CVImagePreprocessor().adjust_contrast(image_bytes, amount=1.2)
 
-	item_number = ai_evaluator.get_response(image_bytes, FIND_ITEM_NUMBER_PROMPT, "")
+	item_number = AIAnswerEvaluator().get_response(image_bytes, FIND_ITEM_NUMBER_PROMPT, "")
 	print("--> ITEM NUMBER:", item_number)
 
-	response = ai_evaluator.get_response(image_bytes, system_prompt, user_prompt)
+	response = AIAnswerEvaluator().get_response(image_bytes, SYSTEM_PROMPT, user_prompt)
 	print("--> RESPONSE:", response)
