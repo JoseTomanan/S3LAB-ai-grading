028e055f8b685923cda1755c8245259955f3c394
HoweZae <jdtomanan@up.edu.ph>
2026-01-31 19:19:22 +0800

    feat: init papers page, AddTestItem page

---
+++ app/frontend/src/lib/components/dialogs/AddTestItem.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+	import type { TestItem } from '$lib/types/types.ts';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+
+	let formItemLabel: string = "";
+	let formItemQuestion: string = "";
+	let formItemIsProblemSolving: boolean = false;
+	let formItemEARQ: string = "";
+
+	async function addTestItem() {
+		// TODO: finish
+		try {
+		} catch (e) {
+		}
+	}
+</script>
+
+<Dialog.Content>
+	<Dialog.Header>
+		<Dialog.Title>Add new test item</Dialog.Title>
+	</Dialog.Header>
+	<!-- TODO: finish -->
+	<Dialog.Footer>
+		<button class="button-primary" onclick={() => addTestItem()}>
+			Add item
+		</button>
+	</Dialog.Footer>
+</Dialog.Content>
---
+++ app/frontend/src/routes/[test_id]/papers/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	
+</script>
+
+<h3>Bayaw</h3>
