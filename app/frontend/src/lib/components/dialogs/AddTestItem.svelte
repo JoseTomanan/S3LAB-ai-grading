04742b65c8d452c2742beeba634c3c76488e3f3d
HoweZae <jdtomanan@up.edu.ph>
2026-02-01 14:27:23 +0800

    feat(frontend): add (semi-working) placeholders for function, form

--- app/frontend/src/lib/components/dialogs/AddTestItem.svelte
+++ app/frontend/src/lib/components/dialogs/AddTestItem.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
 	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
+	let { test_id } = $props();
+
 	import type { TestItem } from '$lib/types/types.ts';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.ts';
 
-	let formItemLabel: string = "";
-	let formItemQuestion: string = "";
-	let formItemIsProblemSolving: boolean = false;
-	let formItemEARQ: string = "";
+	let formItemLabel: string = $state("");
+	let formItemQuestion: string = $state("");
+	let formItemIsProblemSolving: boolean = $state(false);
+	let formItemEARQ: string = $state("");
 
 	async function addTestItem() {
 		// TODO: finish
 		try {
+			const response = await fetch(
+				`${apiBaseUrl}/api/test_instances/${test_id}/items`,
+				{
+					method: "POST",
+					headers: {'Content-Type': 'application/json',},
+					body: JSON.stringify({}),
+				}
+			);
+
+			alert(response.status + response.statusText)
 		} catch (e) {
+			alert("Failed completing operation, check your connection and try again.")
 		}
 	}
 </script>
@@ -24,6 +38,15 @@
 		<Dialog.Title>Add new test item</Dialog.Title>
 	</Dialog.Header>
 	<!-- TODO: finish -->
+	<Label for="item_id">Item label (item_id)</Label>
+	<Input id="item_id" bind:value={formItemLabel} />
+	<Label for="item_id">Question</Label>
+	<Textarea id="item_id" rows={4} bind:value={formItemQuestion} />
+	<Label for="is_problem_solving">Type of question</Label>
+	<Input id="is_probem_solving" type="radio" name="short">Short form</Input>
+	<Input id="is_probem_solving" type="radio" name="problem">Problem solving</Input>
+	<Label for="e_a_r_q">Expected answer</Label>
+	<Textarea id="e_a_r_q" rows={6} bind:value={formItemEARQ} />
 	<Dialog.Footer>
 		<button class="button-primary" onclick={() => addTestItem()}>
 			Add item
