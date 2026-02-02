0195b9420248b8c25c3938bd29caffe8f7d6dc22
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-02 20:39:53 +0800

    feat: add redirect

---
+++ app/frontend/src/routes/[test_id]/+page.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types.d.ts';
+
+export const load: PageLoad = ({ params }) => {
+	// This triggers as soon as the user hits /[test_id]
+	throw redirect(308, `/${params.test_id}/items`);
+};
