f0488c614b7e2a57843b1ba72b0732558772a0c1
HoweZae <jdtomanan@up.edu.ph>
2026-02-02 19:51:49 +0800

    feat: make dialog more single-purpose; create function placeholders

--- app/frontend/src/lib/components/dialogs/ProcessImage.svelte
+++ app/frontend/src/lib/components/dialogs/ProcessImage.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
 	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+	
+	const { student_no, studentItem }: { student_no: string, studentItem: (StudentAnswer & {label: string}) } = $props();
 
-	import MdiPlus from '~icons/mdi/plus';
-
-	import type { Student, TestItemHeader } from '$lib/types/types.ts';
+	import type { Student, StudentAnswer } from '$lib/types/types.ts';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 
-	let { student, itemHeaders }: { student: Student, itemHeaders: TestItemHeader[] } = $props();
+	async function recrop() {
+		// TODO: function
+	}
+
+	async function sendImage() {
+		// TODO: function
+	}
 </script>
 
-<Dialog.Content>
-	<Dialog.Title>For {student.name}</Dialog.Title>
-	{#each itemHeaders as item}
-		<span class="flex flex-row gap-4">
-			<Label for={student.student_no}>{item.label}</Label>
-			<button class="button-secondary">
-				<MdiPlus />
-			</button>
-		</span>
-		<!-- TODO: forms, image cropping interface, etc. -->
-	{/each}
+
+<Dialog.Content class="max-h-[90vh] overflow-y-auto">
+	<Dialog.Title>{student_no}: Item {studentItem.label}</Dialog.Title>
+	<button class="button-secondary">Send/replace image</button>
+	<button class="button-secondary">Recrop</button>
 </Dialog.Content>
+
+<!-- TODO: forms, image cropping interface, etc. -->
