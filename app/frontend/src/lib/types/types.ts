5c3980a2f48edea1945112607a3ad8ba1bab7174
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-02 09:54:18 +0800

    feat: create types (according to relational schema)

--- app/frontend/src/lib/types/types.ts
+++ app/frontend/src/lib/types/types.ts
@@ -1,28 +1,47 @@
-// TODO: segregate into different files (soon)
-
-export enum RenderStatus {
-	INCOMPLETE = "Incomplete",
-	IN_PROGRESS = "In progress",
-	DONE = "Done",
-}
-
 export interface TestInstance {
+	test_id: string;
 	name: string;
 	section: string;
 	date: string;
+	is_done_rendering: boolean;
+}
+
+
+export interface TestItem {
+	item_id: number;
+	label: string;
+	question: string;
+	is_problem_solving: boolean;
+	expected_answer_rubric_questions: string;
+}
+
+
+export interface TestPaper {
+	paper_id: number;
+	student_no: string;
 	test_id: string;
 	is_done_rendering: boolean;
 }
 
-export interface TestItem {
-	item_id: string;
-	question: string;
-	is_problem_solving: boolean;
-	expected_answer_rubric_questions: string;	// if rubric question, separate with `. `
+
+export interface Student {
+	student_no: string;
+	name: string;
+	section: string;
 }
 
-export interface TestPaper {
-	paper_id: string;
+
+export interface StudentAnswer {
+	answer_id: string;
 	student_no: string;
-	// TODO: add more; I'm definitely forgetting something.
+	item_id: string;
+	image_directory: string;
+	ai_evaluation: string;
+	is_done_rendering: boolean;
+}
+
+
+export interface Section {
+	section_id: number;
+	section: string;
 }
