7231ce3abac0f150c3773081762fcb673e861202
HoweZae <jdtomanan@up.edu.ph>
2026-02-03 21:38:02 +0800

    feat: add API handling; add variable for file; segregate cropping to different component

---
+++ app/frontend/src/lib/components/dialogs/ManualCrop.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+	
+	let manualCropPoints: {x: number, y: number}[] = $state([
+				{x: 0, y: 0}, {x: 25, y: 0}, {x: 25, y: 25}, {x: 0, y: 25}, 
+				]);
+
+	async function recrop() {
+		// TODO: function
+	}
+</script>
+
+
--- app/frontend/src/lib/components/dialogs/ProcessImage.svelte
+++ app/frontend/src/lib/components/dialogs/ProcessImage.svelte
@@ -1,26 +1,49 @@
 <script lang="ts">
 	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 	
-	const { student_no, studentItem }: { student_no: string, studentItem: (StudentAnswer & {label: string}) } = $props();
+	const { test_id, student_no, studentItem }: { test_id: string, student_no: string, studentItem: (StudentAnswer & {label: string}) } = $props();
 
 	import type { Student, StudentAnswer } from '$lib/types/types.ts';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 
-	async function recrop() {
-		// TODO: function
-	}
+	let formFile: FileList | undefined = $state();
 
 	async function sendImage() {
-		// TODO: function
+		if (!formFile || formFile.length == 0)
+			return;
+
+		const formData = new FormData();
+    formData.append('image', formFile[0]);
+
+		try {
+			const response = await fetch(
+				`${apiBaseUrl}/test_instances/cvprocess`,
+				{ method: "POST", body: formData, }
+			);
+
+			if (response.ok) {
+				alert("Sending is successful.");
+				window.location.reload();
+			} else {
+				alert(response.status + response.statusText);
+			}
+		} catch(e) {
+			alert("Failed to fetch, check your network connection and try again.\n"+e);
+		} finally {
+		}
 	}
 </script>
 
 
 <Dialog.Content class="max-h-[90vh] overflow-y-auto">
-	<Dialog.Title>{student_no}: Item {studentItem.label}</Dialog.Title>
-	<button class="button-secondary">Send/replace image</button>
-	<button class="button-secondary">Recrop</button>
+	<Dialog.Title>Send image</Dialog.Title>
+	<Dialog.Description>{test_id} &middot; {student_no} &middot; Item {studentItem.label}</Dialog.Description>
+	<Input id="sendImage" type="file" accept="image/*" bind:files={formFile}/>
+	<Button variant="default"  onclick={() => sendImage()} disabled={!formFile}>
+		Send for processing
+	</Button>
 </Dialog.Content>
 
-<!-- TODO: forms, image cropping interface, etc. -->
