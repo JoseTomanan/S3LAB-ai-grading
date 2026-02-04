bf0bdb126595e7c02811b95b3c31cf646aa7646f
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-04 23:30:50 +0800

    feat: add manual cropping page (not dialog)

---
+++ app/frontend/src/routes/[test_id]/papers/[student_no]/manual/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+	const { data } = $props();
+</script>
+
+<div class="flex flex-col space-y-2">
+	<h2 class="font-bold">Manually crop image</h2>
+	<h6>{data.test_id} &middot; {data.student_no} &middot; ...</h6>
+	<!-- TODO: all -->
+</div>
