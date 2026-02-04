26fc79a86e739d918cceda7cf51231975c86c4dc
HoweZae <jdtomanan@up.edu.ph>
2026-02-04 15:28:55 +0800

    feat: add placeholders (non-functional)

--- app/frontend/src/lib/components/dialogs/ManualCrop.svelte
+++ app/frontend/src/lib/components/dialogs/ManualCrop.svelte
@@ -1,13 +1,48 @@
 <script lang="ts">
 	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 	
+	const { test_id, student_no, studentItem }: { test_id: string, student_no: string, studentItem: (StudentAnswer & {label: string}) } = $props();
+
+	import type { Student, StudentAnswer } from '$lib/types/types.ts';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+
+	let isOperationOngoing: boolean = $state(false);
+
+	let formFile: FileList | undefined = $state();
+
 	let manualCropPoints: {x: number, y: number}[] = $state([
 				{x: 0, y: 0}, {x: 25, y: 0}, {x: 25, y: 25}, {x: 0, y: 25}, 
 				]);
 
-	async function recrop() {
+	async function sendCropRequest() {
 		// TODO: function
 	}
 </script>
 
 
+<Dialog.Content>
+	<Dialog.Title>Manually crop image</Dialog.Title>
+	<Dialog.Description>{test_id} &middot; {student_no} &middot; Item {studentItem.label}</Dialog.Description>
+	<Input id="sendCropRequest" type="file" accept="image/*" bind:files={formFile}/>
+	{#if formFile}
+		<div class="container">
+			<!-- TODO: create SVG canvas interface for picking 4 points -->
+		</div>
+	{:else}
+		<p>Please upload an image.</p>
+	{/if}
+	<Button variant="default" onclick={() => sendCropRequest()} disabled={!formFile}>
+		Send crop request
+	</Button>
+	{#if isOperationOngoing}
+		<p>Loading...</p>
+	{/if}
+</Dialog.Content>
+
+
+<style>
+	/* TODO: add styles (take from Gemini's input) */
+</style>
