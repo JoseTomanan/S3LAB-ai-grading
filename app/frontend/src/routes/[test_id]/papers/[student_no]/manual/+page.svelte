fbab73647bd89d95a76629a828a2d99e4adcdc82
HoweZae <jdtomanan@up.edu.ph>
2026-02-05 12:14:42 +0800

    feat: put the easy stuff in

--- app/frontend/src/routes/[test_id]/papers/[student_no]/manual/+page.svelte
+++ app/frontend/src/routes/[test_id]/papers/[student_no]/manual/+page.svelte
@@ -2,10 +2,45 @@
 	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 	const { data } = $props();
+
+	import { Input } from '$lib/components/ui/input/index.ts';
+	import { Button } from '$lib/components/ui/button/index.ts';
+
+	let canvasFile: FileList | undefined = $state();
+	let canvasImageUrl: string | null = $state(null);
+	let canvasRectangle: {x: number, y: number, width: number, height: number};
+	
+	let returnablePoints: {x: number, y: number}[] = [];
+
+	function handleFileUpload(e: Event) {
+		const target = e.target as HTMLInputElement;
+		const file = target.files?.[0];
+
+		if (file) {
+			if (canvasImageUrl)
+				URL.revokeObjectURL(canvasImageUrl);
+			canvasImageUrl = URL.createObjectURL(file);
+		}
+	}
+
+	async function sendCropRequest() {
+		// TODO: function
+	}
 </script>
+
 
 <div class="flex flex-col space-y-2">
 	<h2 class="font-bold">Manually crop image</h2>
 	<h6>{data.test_id} &middot; {data.student_no} &middot; ...</h6>
+	<Input id="croppable"
+				type="file" accept="image/*"
+				bind:files={canvasFile}
+				onchange={handleFileUpload}/>
+	{#if canvasFile}
 	<!-- TODO: all -->
+	{/if}
+	<Button variant="secondary" disabled={!canvasFile}
+				onclick={() => sendCropRequest()}>
+		Send crop request
+	</Button>
 </div>
