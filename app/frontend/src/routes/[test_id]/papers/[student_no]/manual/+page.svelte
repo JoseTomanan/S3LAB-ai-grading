4454cf8db57a76f0f14aaeb837b0661dba08324d
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-05 13:15:50 +0800

    feat(frontend): use svelte-cropper to implement image handling, request handling

--- app/frontend/src/routes/[test_id]/papers/[student_no]/manual/+page.svelte
+++ app/frontend/src/routes/[test_id]/papers/[student_no]/manual/+page.svelte
@@ -3,14 +3,15 @@
 
 	const { data } = $props();
 
+	import { Cropper, type CropperInstance } from "svelte-cropper";
 	import { Input } from '$lib/components/ui/input/index.ts';
 	import { Button } from '$lib/components/ui/button/index.ts';
 
+	let isOperationOngoing: boolean = $state(false);
+
+	let canvasCropper: CropperInstance | null = $state(null);
 	let canvasFile: FileList | undefined = $state();
 	let canvasImageUrl: string | null = $state(null);
-	let canvasRectangle: {x: number, y: number, width: number, height: number};
-	
-	let returnablePoints: {x: number, y: number}[] = [];
 
 	function handleFileUpload(e: Event) {
 		const target = e.target as HTMLInputElement;
@@ -24,7 +25,42 @@
 	}
 
 	async function sendCropRequest() {
-		// TODO: function
+		isOperationOngoing = true;
+
+		const canvasRectangle: {x: number, y: number, width: number, height: number} = canvasCropper!.getData();
+		const returnablePoints: {x: number, y: number}[] = [
+				{ x: canvasRectangle.x, y: canvasRectangle.y },																								// Top-Left
+				{ x: canvasRectangle.x+canvasRectangle.width, y: canvasRectangle.y },													// Top-Right
+				{ x: canvasRectangle.x+canvasRectangle.width, y: canvasRectangle.y+canvasRectangle.height },	// Bottom-Right
+				{ x: canvasRectangle.x, y: canvasRectangle.y+canvasRectangle.height }													// Bottom-Left
+				];
+
+		console.log("POINTS TO PASS: "+returnablePoints);
+
+		const formData: FormData = new FormData();
+		formData.append('image', canvasFile![0]);
+
+		const formMetadata = { points: {
+					ul: returnablePoints[0],
+					ur: returnablePoints[1],
+					lr: returnablePoints[2],
+					ll: returnablePoints[3],
+					}};
+		formData.append('metadata', JSON.stringify(formMetadata));
+
+		try {
+			const response = await fetch(
+						`${apiBaseUrl}/test_instances/${data.test_id}/${data.student_no}`,
+						{ method: "PATCH", body: formData, }
+						);
+
+			const result = await response.json();
+			canvasImageUrl = result.image_directory;
+		} catch(e) {
+			console.log("Failed to finish, check your network connection and try again.\n"+e);
+		} finally {
+			isOperationOngoing = false;
+		}
 	}
 </script>
 
@@ -35,11 +71,19 @@
 	<Input id="croppable"
 				type="file" accept="image/*"
 				bind:files={canvasFile}
-				onchange={handleFileUpload}/>
-	{#if canvasFile}
-	<!-- TODO: all -->
+				onchange={handleFileUpload}
+				/>
+	{#if canvasImageUrl}
+		<Cropper bind:cropper={canvasCropper}
+					src={canvasImageUrl}
+					cropper_props={{viewMode: 2, dragMode: "crop", initialAspectRatio: 1}}
+					/>
 	{/if}
-	<Button variant="secondary" disabled={!canvasFile}
+	{#if isOperationOngoing}
+		<p>Loading...</p>
+	{/if}
+	<Button variant="secondary"
+				disabled={!canvasFile || !canvasImageUrl || isOperationOngoing}
 				onclick={() => sendCropRequest()}>
 		Send crop request
 	</Button>
