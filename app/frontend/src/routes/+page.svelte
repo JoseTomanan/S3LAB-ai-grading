0a62150edfcae2869a71daa4afb74c0593010f7b
HoweZae <jdtomanan@up.edu.ph>
2026-02-09 19:08:29 +0800

    refactor: spacing; minor

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -20,21 +20,17 @@
 	onMount(async () => {
 		try {
 			const response = await fetch(
-				`${API_BASE_URL}/api/test_instances`,
-				{
-					method: "GET",
-					headers: {'Content-Type': 'application/json',}
-				}
-			);
+						`${API_BASE_URL}/api/test_instances`,
+						{
+							method: "GET",
+							headers: {'Content-Type': 'application/json',}
+						}
+						);
 
 			switch (response.status) {
 				case 200:
 					const result = await response.json();
-					// console.log(result);
-					if (result) {
-						// console.log("--> RESULT INSTANCES EXIST.");
-						instances = result;
-					}
+					instances = result ? result : [];
 					break;
 				default:
 					alert(`${response.status} ${response.statusText}`);
