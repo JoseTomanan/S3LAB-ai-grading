770e1da5dbc1fafaa38b22178447aff905ebba6b
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-11 20:59:23 +0800

    feat: segregate question string from points

--- app/frontend/src/routes/[test_id]/items/AddTestItem.svelte
+++ app/frontend/src/routes/[test_id]/items/AddTestItem.svelte
@@ -2,6 +2,7 @@
 	const { test_id } = $props();
 	
 	import { API_BASE_URL } from '$lib/constants.ts';
+	import MdiPlus from "~icons/mdi/plus";
 
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import * as RadioGroup from '$lib/components/ui/radio-group/index.ts';
@@ -13,10 +14,21 @@
 	let formItemLabel: string = $state("");
 	let formItemQuestion: string = $state("");
 	let formItemIsProblemSolving: boolean = $state(false);
-	let formItemEARQ: string = $state("");
+	let formItemRQ: {question: string, points: number}[] = $state([{question: "", points: 0}]);
+	let formItemEA: {question: string, points: number} = $state({question: "", points: 0});
+	
+	let submittableEARQ: string = "";
 
 	async function addTestItem() {
-		// FIXME: remove this once verified that functional
+		if (formItemIsProblemSolving) {
+			for (const item of formItemRQ) {
+				if (item.points != 0)
+					submittableEARQ = submittableEARQ.concat(`${item.question} [${item.points}pts];`);
+			}
+		} else {
+			submittableEARQ = `${formItemEA.question} [${formItemEA.points}pts]`
+		}
+
 		try {
 			const response = await fetch(
 				`${API_BASE_URL}/api/test_instances/${test_id}/items`,
@@ -27,7 +39,7 @@
 						label: formItemLabel,
 						question: formItemQuestion,
 						is_problem_solving: formItemIsProblemSolving,
-						expected_answer_rubric_questions: formItemEARQ,
+						expected_answer_rubric_questions: submittableEARQ,
 					}),
 				}
 			);
@@ -65,12 +77,39 @@
 			<Label for="prob_sol" class="font-normal">Problem solving</Label>
 		</span>
 	</RadioGroup.Root>
-	<Label for="e_a_r_q">
-		{formItemIsProblemSolving ? "Rubric questions (separate with `. `)" : "Expected answer"}
-	</Label>
-	<Textarea id="e_a_r_q" rows={6} bind:value={formItemEARQ} />
+	{#if formItemIsProblemSolving}
+		<Label for="r_q"
+						class="flex flex-row justify-between">
+			Rubric questions
+			<button class="px-1 py-0 m-0 bg-secondary"
+							onclick={() => {formItemRQ.push({question: "", points: 0})}}>
+				<MdiPlus class="size-3"/>
+			</button>
+		</Label>
+		<div class="space-y-1">
+			{#each formItemRQ as item}
+				<span class="flex flex-row gap-1">
+					<Input id="r_q" class="w-5/6"
+									bind:value={item.question} />
+					<Input id="r_q" class="w-1/6"
+									type="number"
+									bind:value={item.points} />
+				</span>
+			{/each}
+		</div>
+	{:else}
+		<Label for="e_a">Expected answer</Label>
+		<span class="flex flex-row gap-1">
+			<Input id="e_a" class="w-5/6"
+							bind:value={formItemEA.question} />
+			<Input id="e_a" class="w-1/6"
+							type="number"
+							bind:value={formItemEA.points} />
+		</span>
+	{/if}
 	<Dialog.Footer>
-		<Button variant="outline" onclick={() => addTestItem()}>
+		<Button variant="outline"
+						onclick={() => addTestItem()}>
 			Add item
 		</Button>
 	</Dialog.Footer>
