b1ea0723c258941b8f200d037fc8fab15a999ec3
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-18 23:04:41 +0800

    feat(frontend): create schemas

--- app/frontend/src/lib/index.ts
+++ app/frontend/src/lib/index.ts
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export type * from './types/types.ts';
 export type * from './types/ui.ts';
+export type * from './types/schemas.ts';
