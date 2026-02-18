b97c6b6383bc72a06cd2db46fb3860967712ace5
HoweZae <jdtomanan@up.edu.ph>
2026-02-18 18:21:44 +0800

    feat(frontend): use imports in error page

--- app/frontend/src/routes/+error.svelte
+++ app/frontend/src/routes/+error.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	import { page } from "$app/state";
+</script>
+
+
 <div class="px-4 py-8 flex flex-col gap-4">
-	<h1>404 LINK NOT FOUND</h1>
+	<h1>{page.status} {page.error?.message ?? ""}</h1>
 	<h4>
 		Please check the link you are visiting. <br>
 		If you are attempting to visit a custom link, please start from the following: <br>
