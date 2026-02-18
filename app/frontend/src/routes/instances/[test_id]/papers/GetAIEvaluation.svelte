edd1cd5411c777c364b031b0689ec1504412062e
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-18 23:55:11 +0800

    feat(frontend): add fetcher code for AI evaluations of individual student

--- app/frontend/src/routes/instances/[test_id]/papers/GetAIEvaluation.svelte
+++ app/frontend/src/routes/instances/[test_id]/papers/GetAIEvaluation.svelte
@@ -4,14 +4,35 @@
 	import { API_BASE_URL } from '$lib/constants.ts';
 	import { onMount } from "svelte";
 
+	import type { EvaluationsResponse, StudentStoresResponse } from '$lib/index.ts';
 	import * as Dialog from "$lib/components/ui/dialog/index.ts";
 
 	let isPageLoading = $state(false);
+	let questionItemEvals: EvaluationsResponse[] = $state([]);
 
 	onMount(async () => {
+		isPageLoading = true;
 		try {
+			const response = await fetch(
+						`${API_BASE_URL}/api/test_instances/${test_id}/results/${student_no}`,
+						{
+							method: "GET",
+							headers: {'Content-Type': 'application/json',},
+						}
+						);
+
+			switch (response.status) {
+				case 200:
+					const result = await response.json();
+					questionItemEvals = result.evaluations;
+					break;
+				default:
+					alert(`${response.status} ${response.statusText}`);
+			}
 		} catch (e) {
+			alert("Failed to fetch, check your network connection and try again.\nERROR: "+e);
 		} finally {
+			isPageLoading = false;
 		}
 	});
 </script>
@@ -22,5 +43,16 @@
 		<Dialog.Title>AI evaluation results</Dialog.Title>
 		<Dialog.Description>{test_id} &middot; {student_no}</Dialog.Description>
 	</Dialog.Header>
-	<!-- TODO: content -->
+	<div class="max-h-[80vh] overflow-auto">
+		{#each questionItemEvals as evalItem}
+			<div class="card space-y-1">
+				<h3>{ evalItem.label }</h3>
+				<h6 class="truncate text-ellipsis w-fill">
+					{ evalItem.question }
+				</h6>
+				{evalItem.expected_answer_rubric_questions}
+				{evalItem.ai_evaluation}
+			</div>
+		{/each}
+	</div>
 </Dialog.Content>
