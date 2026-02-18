67e8d09326faadb37d0f82b7b2e658a3aa007c1d
HoweZae <jdtomanan@up.edu.ph>
2026-02-18 18:18:25 +0800

    feat(frontend): 404 page

---
+++ app/frontend/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<div class="px-4 py-8 flex flex-col gap-4">
+	<h1>404 LINK NOT FOUND</h1>
+	<h4>
+		Please check the link you are visiting. <br>
+		If you are attempting to visit a custom link, please start from the following: <br>
+		<a href="/">Home</a> &middot;
+		<a href="/sections">Sections</a>
+	</h4>
+</div>
+
+
+<style>
+	a {
+		@apply underline;
+	}
+</style>
