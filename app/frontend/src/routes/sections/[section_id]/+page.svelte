4837481b2a80b680b791eb6381ba49c77c4c82fc
HoweZae <jdtomanan@up.edu.ph>
2026-02-19 13:19:21 +0800

    feat(frontend): add content for given students

--- app/frontend/src/routes/sections/[section_id]/+page.svelte
+++ app/frontend/src/routes/sections/[section_id]/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { API_BASE_URL } from '$lib/constants.ts';
-	import type { Student } from '$lib/index.ts';
-	const { data } = $props();
-
 	import { onMount } from 'svelte';
-
+	
+	const { data } = $props();
+	
+	import MdiEdit from "~icons/mdi/edit";
+	import type { Student } from '$lib/index.ts';
+	import * as Dialog from '$lib/components/ui/dialog/index.ts';
+	
+	let isPageLoading = $state(true);
 	let students: Student[] = $state([]);
 	
 	onMount(async () => {
@@ -20,7 +24,6 @@
 			switch (response.status) {
 				case 200:
 					const result = await response.json();
-					console.log(result);
 					students = result;
 					break;
 				default:
@@ -29,27 +32,45 @@
 		} catch (e) {
 			alert("Failed to fetch section details, students:\nERROR: "+e);
 		} finally {
-
+			isPageLoading = false;
 		}
 	}); 
 </script>
 
 
 <div class="px-4 py-8 flex flex-col gap-4">
-	<span class="flex flex-row justify-between">
-		<h1>SECTION_ID {data.section_id}</h1>
+	<span class="flex flex-row space-x-4 mb-4">
 		<a href="/sections"
 				class="font-bold button-outline">
 			&middot;&middot;&middot;
 		</a>
+		<h1 class="align-left">
+			From SECTION_ID {data.section_id}
+		</h1>
 	</span>
-	<!-- TODO: add new student functionality -->
-	<!-- TODO: edit student functionality -->
-	<!-- TODO: delete student functionaity -->
-	{#each students as student}
-		<div class="card">
-			<h3>{student.name}</h3>
-			<h5>{student.student_no}</h5>
-		</div>
-	{/each}
+	<Dialog.Root>
+		<Dialog.Trigger class="button-outline font-medium items-center">
+			(+) Add new student
+		</Dialog.Trigger>
+		<!-- TODO: AddNewStudent functionality -->
+	</Dialog.Root>
+	{#if isPageLoading}
+		<p>Loading...</p>
+	{:else}
+		{#each students as student}
+			<div class="card">
+				<span class="flex flex-row items-center justify-between">
+					<h3>{student.name}</h3>
+					<Dialog.Root>
+						<Dialog.Trigger class="button-secondary">
+							<MdiEdit class="size-4"/>
+						</Dialog.Trigger>
+						<!-- TODO: EditStudent functionality -->
+						<!-- TODO: delete student call -->
+					</Dialog.Root>
+				</span>
+				<h5>{student.student_no}</h5>
+			</div>
+		{/each}
+	{/if}
 </div>
