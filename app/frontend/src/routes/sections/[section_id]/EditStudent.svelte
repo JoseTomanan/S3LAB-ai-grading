5af2934f637ada448ee840c907a49344f1b06c1f
JoseTomanan <jdtomanan@up.edu.ph>
2026-02-19 23:14:35 +0800

    feat(frontend): add sections dropdown for EditStudent

--- app/frontend/src/routes/sections/[section_id]/EditStudent.svelte
+++ app/frontend/src/routes/sections/[section_id]/EditStudent.svelte
@@ -1,17 +1,55 @@
 <script lang="ts">
 	import { API_BASE_URL } from '$lib/constants.ts';
+	import { onMount } from 'svelte';
 
 	const { section_id, student_no, name } = $props();
 
 	import MdiDelete from "~icons/mdi/delete";
 
 	import * as Dialog from "$lib/components/ui/dialog/index.ts";
+	import * as Select from "$lib/components/ui/select/index.ts";
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Input } from '$lib/components/ui/input/index.ts';
 	import { Label } from '$lib/components/ui/label/index.ts';
+	
+	import type { Section } from '$lib/index.ts';
+
+	let isSectionsLoading = $state(false);
+	let dropdownSections: Section[] = $state([]);
 
 	let isLoading = $state(false);
 	let formName: string = $state(name);
+	let formSectionId: string = $state(section_id.toString())
+
+	const triggerContent = $derived(
+				dropdownSections.find((f) => f.section_id.toString() == formSectionId)?.section_name ?? "Section..."
+				);
+
+	onMount(async () => {
+		isSectionsLoading = true;
+		try {
+			const response = await fetch(
+						`${API_BASE_URL}/api/sections`,
+						{
+							method: "GET",
+							headers: {'Content-Type': 'application/json',}
+						}
+						);
+
+			switch (response.status) {
+				case 200:
+					const result = await response.json();
+					dropdownSections = result;
+					break;
+				default:
+					alert(`${response.status} ${response.statusText}`);
+			}
+		} catch (e) {
+			alert("Failed to fetch sections.\n"+e);
+		} finally {
+			isSectionsLoading = false;
+		}
+	});
 
 	async function editStudent() {
 		if (formName == name) {
@@ -40,6 +78,22 @@
 	<Input id="name"
 					placeholder="Name..."
 					bind:value={formName} />
+	<Label id="section">Section</Label>
+	<Select.Root type="single"
+								name="section"
+								bind:value={formSectionId}>
+		<Select.Trigger class="w-full text-base md:text-sm">
+			{triggerContent}
+		</Select.Trigger>
+		<Select.Content>
+			{#each dropdownSections as s (s.section_id)}
+				<Select.Item value={s.section_id.toString()}
+											label={s.section_name}>
+					{s.section_name} ({s.section_id})
+				</Select.Item>
+			{/each}
+		</Select.Content>
+	</Select.Root>
 	<Dialog.Footer>
 		{#if isLoading}
 			<p>Loading...</p>
