7c8eb654cd8eb0bc053b2dacf2aed6b857db0437
JoseTomanan <jdtomanan@up.edu.ph>
2026-03-03 00:52:22 +0800

    chore(backend): clean up imports and remove unused service initializations

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,20 +1,11 @@
-from fastapi import FastAPI, HTTPException, Response, status, Depends, File, UploadFile, Form, Query
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import Session, delete, select
-from typing import List, Optional
 
-import uuid
-import json
 from pathlib import Path
 
 from models import *
 from schemas import *
 from database import create_db_and_tables, get_session
-
-from services.ai_interface import AIAnswerEvaluator
-from services.box_segmenter import BoxSegmenter
-from services.document_scanner import DocumentScanner
-from services.image_modifier import ImageModifier
 
 from services.utility import *
 
@@ -22,7 +13,6 @@ from routers import sections, students, utility, test_instances, test_items, stu
 
 
 
-# ==============================
 #region App Initialization
 app = FastAPI(
         title="Assessment Processing API",
@@ -39,15 +29,9 @@ app.add_middleware(
         allow_headers=["*"],
         )
 
-AI_ANSWER_EVALUATOR = AIAnswerEvaluator()
-DOCUMENT_SCANNER = DocumentScanner()
-IMAGE_MODIFIER = ImageModifier()
-
 TEMP_DIR = Path("static/images")
 TEMP_DIR.mkdir(exist_ok=True)
-
 #endregion
-# ==============================
 
 
 app.include_router(test_instances.router, prefix="/api/test_instances", tags=["Test Instances"])
@@ -56,6 +40,4 @@ app.include_router(students.router, prefix="/api/students", tags=["Students"])
 app.include_router(student_answers.router, prefix="/api/student_answers", tags=["Student Answers"])
 app.include_router(sections.router, prefix="/api/sections", tags=["Sections"])
 app.include_router(utility.router, prefix="/api", tags=["Utility"])
-
-
 
