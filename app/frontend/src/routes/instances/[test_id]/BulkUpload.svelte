18337adca1beff1295cec0289bdb3e94865b1f23
HoweZae <jdtomanan@up.edu.ph>
2026-03-06 17:13:03 +0800

    feat(frontend): better overflowing

--- app/frontend/src/routes/instances/[test_id]/BulkUpload.svelte
+++ app/frontend/src/routes/instances/[test_id]/BulkUpload.svelte
@@ -4,6 +4,7 @@
   const { test_id } = $props();
 
   import IconSend from "~icons/mdi/send";
+  import IconPerson from "~icons/mdi/person";
 
   import * as Dialog from '$lib/components/ui/dialog/index.ts';
   import { Button } from '$lib/components/ui/button/index.ts';
@@ -12,6 +13,10 @@
 
   let files: File[] = $state([]);
   let previews: {name: string, url: string}[] = $state([]);
+
+  $effect(() => {
+    // TODO: filename is according to inputted student number + file extension
+  });
 
   function handleFiles(e: Event) {
     const input = e.target as HTMLInputElement;
@@ -32,7 +37,7 @@
 </script>
 
 
-<Dialog.Content class="min-w-0 *:min-w-0">
+<Dialog.Content class="w-full *:min-w-0">
   <Input type="file"
           multiple
           accept="image/*"
@@ -58,21 +63,27 @@
           <IconSend class="size-4" />
         </Button>
       </Dialog.Header>
-      <div class="flex flex-row items-center overflow-x-auto gap-x-1 pt-1 pb-3
+      <div class="flex flex-row items-center overflow-x-auto gap-x-1
+              -mx-5 px-5 pt-1 pb-2 shadow-sm
               border-b-2 border-t-2 border-border"
            style="scrollbar-gutter: stable; overflow-y: hidden;">
         {#each previews as p}
           {@const supposedId = p.name.substring(0, p.name.lastIndexOf('.'))}
-          <div class="relative flex flex-col justify-end min-w-2/3 max-w-2/3 gap-y-0.5">
+          {@const fileExtension = p.name.substring(p.name.lastIndexOf('.'))}
+          <div class="relative flex flex-col justify-end min-w-full sm:min-w-3/4 h-auto gap-y-0.5">
             <img src={p.url}
-                  class="aspect-auto w-full h-auto block"
+                  class="aspect-auto block"
                   alt={p.name} />
             <span class="absolute bottom-0 left-0 w-full flex flex-row pl-2">
               <Dialog.Root>
-                <Dialog.Trigger class="max-w-full truncate px-1.5 mb-1 bg-white/80 backdrop-blur-md hover:underline cursor-pointer">
-                  <h4 class="text-left w-fit truncate">ID: {supposedId}</h4>
+                <Dialog.Trigger class="max-w-full flex flex-row gap-1 items-center truncate px-1.5 mb-1 bg-white/80 backdrop-blur-md 
+                                    hover:underline cursor-pointer">
+                  <IconPerson class="size-3"/>
+                  <h4 class="text-left w-fit truncate">
+                    {supposedId}
+                  </h4>
                 </Dialog.Trigger>
-                <BulkUploadRename />
+                <BulkUploadRename filename={p.name} />
               </Dialog.Root>
             </span>
           </div>
