e8e0c50d199a406de2737a5dcd946e74c892e564
HoweZae <jdtomanan@up.edu.ph>
2026-03-06 11:24:49 +0800

    feat(frontend): make work; add unready indicator

--- app/frontend/src/routes/instances/[test_id]/papers/[student_no]/OpenCamera.svelte
+++ app/frontend/src/routes/instances/[test_id]/papers/[student_no]/OpenCamera.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   let { onImageCapture } = $props();
 
-  import EasyCamera from '@cloudparker/easy-camera-svelte';
-
   import MdiCamera from "~icons/mdi/camera";
   import MdiImageCheck from "~icons/mdi/image-check";
-
+  
+  import Camera from '$lib/components/MobileCamera/index.ts';
   import * as Dialog from '$lib/components/ui/dialog/index.ts';
   import { Button } from '$lib/components/ui/button/index.ts';
+	import { Skeleton } from "$lib/components/ui/skeleton/index.ts";
+	// import { dataUrlToFile, redownloadFile } from "$lib/utils.ts";
 
   let cameraInstance: any = $state(null);
   let capturedImage: string = $state("");
@@ -20,38 +21,54 @@
           isHasCaptured = true;
         };
 
+  const retakeImage = () => {
+          isHasCaptured = false;
+          capturedImage = "";
+          cameraInstance.open();
+        };
+
   const returnImage = () => {
           // redownloadFile(dataUrlToFile(capturedImage, "camera input.jpeg"));
           onImageCapture(capturedImage);
         };
+
+  let isCameraUnready: boolean = $state(false);
 </script>
 
 
 <Dialog.Content class="flex flex-col gap-4 items-center w-full h-full">
+  <Dialog.Title>Capture image</Dialog.Title>
   {#if !isHasCaptured}
-    <Dialog.Title>Capture image</Dialog.Title>
-    <div class="w-full min-h-0 flex justify-center items-center overflow-hidden
-            [&_video]:object-contain [&_video]:max-w-full [&_video]:max-h-full">
-      <EasyCamera bind:this={cameraInstance}
-                  useAudio={false}
-                  width={720}
-                  style="width: 100%; max-height: 100%; object-fit: contain;" />
+    <div class="w-full h-fit flex justify-center items-center overflow-hidden
+            relative">
+      <Camera bind:this={cameraInstance}
+              useAudio={false}
+              useFrontCamera={false}
+              onOpen={() => isCameraUnready = false}
+              onClose={() => isCameraUnready = true}
+          />
+      {#if isCameraUnready}
+        <Skeleton class="absolute grayscale w-full h-full rounded-none"/>
+      {/if}
     </div>
     <Button variant="outline"
               onclick={handleImage}
-              class="px-4 py-2">
-      <MdiCamera />
+              class="px-4 py-2 w-full">
+      <MdiCamera class="size-6"/>
     </Button>
   {:else}
-    <Dialog.Title>Preview image</Dialog.Title>
     <img src={capturedImage}
           alt="Captured preview"
-          class="max-w-full max-h-full w-auto h-auto object-contain rounded-lg ring-4 ring-border shadow" />
-    <Dialog.Close>
-      <button class="button-outline"
-                onclick={returnImage}>
-        <MdiImageCheck />
-      </button>
-    </Dialog.Close>
+          class="border-0 ring-0 shadow" />
+    <div class="flex flex-row w-full gap-1.5">
+      <Dialog.Close class="flex-1 button-primary"
+                  onclick={returnImage}>
+        <MdiImageCheck class="size-6 w-full"/>
+      </Dialog.Close>
+      <Button variant="outline"
+              onclick={retakeImage}>
+        Retake
+      </Button>
+    </div>
   {/if}
 </Dialog.Content>
