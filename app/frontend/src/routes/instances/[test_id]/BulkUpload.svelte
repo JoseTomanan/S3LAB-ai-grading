3333527912266f63f529f377fccf7fe2886ca4f0
HoweZae <jdtomanan@up.edu.ph>
2026-03-06 21:34:39 +0800

    feat(frontend): make updating filename work

--- app/frontend/src/routes/instances/[test_id]/BulkUpload.svelte
+++ app/frontend/src/routes/instances/[test_id]/BulkUpload.svelte
@@ -3,19 +3,25 @@
 
   const { test_id } = $props();
 
+  import IconCheck from "~icons/mdi/check";
   import IconSend from "~icons/mdi/send";
   import IconPerson from "~icons/mdi/person";
 
   import * as Dialog from '$lib/components/ui/dialog/index.ts';
   import { Button } from '$lib/components/ui/button/index.ts';
 	import { Input } from '$lib/components/ui/input/index.ts';
-	import BulkUploadRename from '$lib/components/BulkUploadRename.svelte';
+	import { Spinner } from '$lib/components/ui/spinner/index.ts';
+	import BulkUploadRename from './BulkUploadRename.svelte';
 
   let files: File[] = $state([]);
   let previews: {name: string, url: string}[] = $state([]);
+  let isOperationStarted: boolean = $state(false);
+
+  // Map<index: number, requestStatus: boolean>
+  let renderStatuses: Map<number, boolean> = $state(new Map());
 
   $effect(() => {
-    // TODO: filename is according to inputted student number + file extension
+    console.log(renderStatuses);
   });
 
   function handleFiles(e: Event) {
@@ -29,10 +35,23 @@
       name: f.name,
       url: URL.createObjectURL(f)
     }));
+
+    const newStatuses = new Map<number, boolean>();
+    previews.forEach((_, idx) => newStatuses.set(idx, false));
+    renderStatuses = newStatuses;
   }
 
   async function bulkUpload() {
-    // TODO: function
+    isOperationStarted = true;
+    console.log(renderStatuses);
+
+    let idx = 0;
+    for (const [key, value] of renderStatuses) {
+      console.log(key);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      renderStatuses = new Map(renderStatuses.set(key, true));
+      idx++;
+    }
   }
 </script>
 
@@ -43,12 +62,14 @@
           accept="image/*"
           class="w-11/12"
           onchange={handleFiles}
+          disabled={isOperationStarted}
       />
   {#if files.length == 0}
     <Dialog.Description>
       Make sure files are named with student number, e.g., "202011111.jpeg".
     </Dialog.Description>
-  {:else}
+  
+  {:else if !isOperationStarted}
     <div class="space-y-1">
       <Dialog.Header class="flex flex-row justify-between">
         <span class="block">
@@ -59,7 +80,8 @@
             Uploaded {files.length} images
           </Dialog.Description>
         </span>
-        <Button variant="secondary">
+        <Button variant="secondary"
+                  onclick={bulkUpload}>
           <IconSend class="size-4" />
         </Button>
       </Dialog.Header>
@@ -83,12 +105,30 @@
                     {supposedId}
                   </h4>
                 </Dialog.Trigger>
-                <BulkUploadRename filename={p.name} />
+                <BulkUploadRename
+                      filename={supposedId}
+                      onchange={(studentNo: string) => p.name = studentNo + fileExtension} />
               </Dialog.Root>
             </span>
           </div>
         {/each}
       </div>
+    </div>
+  
+  {:else}
+    <div class="flex flex-col space-y-1">
+      {#each previews as p, idx}
+        <span class="flex flex-row justify-start items-center gap-x-1.5">
+          <span class="mr-1">
+            {#if renderStatuses.get(idx)}
+              <IconCheck class="size-5"/>
+            {:else}
+              <Spinner class="size-5"/>
+            {/if}
+          </span>
+          <h4 class="truncate w-fit">{p.name}</h4>
+        </span>
+      {/each}
     </div>
   {/if}
 </Dialog.Content>
--- app/frontend/src/routes/instances/[test_id]/BulkUploadRename.svelte
+++ app/frontend/src/routes/instances/[test_id]/BulkUploadRename.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-  let { filename = $bindable() } = $props();
+  let { filename, onchange } = $props();
 
-	import { Input } from './ui/input/index.ts';
+	import { Input } from '$lib/components/ui/input/index.ts';
   import * as Dialog from '$lib/components/ui/dialog/index.ts';
-
   import IconCheck from "~icons/mdi/check";
+
+  let newFilename: string = $state(filename);
 </script>
 
 
-<Dialog.Content showCloseButton={false}>
-  <Dialog.Description>Indicate student no. (keep the file extension):</Dialog.Description>
+<Dialog.Content>
+  <Dialog.Description>Change associated student number:</Dialog.Description>
   <div class="flex flex-row gap-x-1">
     <Input type="text"
-            bind:value={filename}
+            bind:value={newFilename}
+            onblur={() => onchange(newFilename)}
             placeholder="Student no..."
         />
     <Dialog.Close class="button-secondary">
