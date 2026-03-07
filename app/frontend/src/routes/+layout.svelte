538a25b7e2fc51029926699861efaccc60fb5b31
HoweZae <jdtomanan@up.edu.ph>
2026-03-07 17:16:45 +0800

    feat(frontend): add state-dependent loading animation

--- app/frontend/src/routes/+layout.svelte
+++ app/frontend/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import '../app.css';
   import favicon from '$lib/assets/favicon.ico';
+	import { navigating } from '$app/state';
+	import Spinner from '$lib/components/ui/spinner/spinner.svelte';
 
   let { children } = $props();
 </script>
@@ -14,4 +16,9 @@
   <div class="big-container">
     {@render children()}
   </div>
+  {#if navigating.to}
+    <footer class="fixed bottom-0 left-0 w-full flex items-start z-50 p-1">
+      <Spinner class="size-8 text-chart-3"/>
+    </footer>
+  {/if}
 </div>
