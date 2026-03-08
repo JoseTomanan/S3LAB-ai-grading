4469482f76104106bd49dee05b717ecadae91d41
HoweZae <jdtomanan@up.edu.ph>
2026-03-08 10:20:32 +0800

    style(frontend): minor

--- app/frontend/src/routes/+layout.svelte
+++ app/frontend/src/routes/+layout.svelte
@@ -9,16 +9,16 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <title>Llamath</title>
+  <title>LLAMATH</title>
 </svelte:head>
 
 <div class="flex justify-center bg-fixed">
   <div class="big-container">
     {@render children()}
   </div>
-  <footer class="fixed bottom-0 left-0 w-full flex items-start z-50 p-1">
+  <footer class="fixed bottom-0 left-0 w-full flex items-start z-50">
     {#if navigating.to}
-      <Spinner class="size-8 text-chart-3"/>
+      <Spinner class="size-8 text-chart-3 p-0.5"/>
     {/if}
   </footer>
 </div>
