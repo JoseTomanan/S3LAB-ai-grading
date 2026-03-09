17ba7a466d139f9c0e0f66d496bc43815ac83049
HoweZae <jdtomanan@up.edu.ph>
2026-03-09 15:57:46 +0800

    feat(frontend): add @vitejs/plugin-basic-ssl

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -7,6 +8,7 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+    basicSsl(),
 		Icons({
 			compiler: 'svelte',
 		}),
