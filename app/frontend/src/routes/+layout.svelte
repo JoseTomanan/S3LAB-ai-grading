268ba00e0bc9a358d348ddedf4ee642e81d49675
HoweZae <jdtomanan@up.edu.ph>
2026-03-09 15:57:32 +0800

    style(frontend): Reflect renaming in brand; make loading icon larger; change color to accent

--- app/frontend/src/routes/+layout.svelte
+++ app/frontend/src/routes/+layout.svelte
@@ -7,18 +7,20 @@
   let { children } = $props();
 </script>
 
+
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <title>LLAMATH</title>
+  <title>SIPAT.MATH</title>
 </svelte:head>
+
 
 <div class="flex justify-center bg-fixed">
   <div class="big-container">
     {@render children()}
   </div>
-  <footer class="fixed bottom-0 left-0 w-full flex items-start z-50">
+  <span class="fixed bottom-0 left-0 w-full flex space-x-2 items-center justify-end z-50 font-semibold p-2">
     {#if navigating.to}
-      <Spinner class="size-8 text-chart-3 p-0.5"/>
+      <Spinner class="size-12 text-accent"/>
     {/if}
-  </footer>
+  </span>
 </div>
