868606d044611c55a8e14ad8ab3989f8d20fe259
HoweZae <jdtomanan@up.edu.ph>
2026-03-09 17:11:46 +0800

    chore(frontend): Set server host to true

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -5,6 +5,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8000',
+    //     changeOrigin: true,
+    //   }
+    // }
+  },
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
