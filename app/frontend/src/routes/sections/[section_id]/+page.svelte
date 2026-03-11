d762be9b704fa7cb5cbe18cb73e4bdd89df1f12c
HoweZae <jdtomanan@up.edu.ph>
2026-03-11 19:09:55 +0800

    style(frontend): minor

--- app/frontend/src/routes/sections/[section_id]/+page.svelte
+++ app/frontend/src/routes/sections/[section_id]/+page.svelte
@@ -9,6 +9,7 @@
 	import * as Dialog from '$lib/components/ui/dialog/index.ts';
 	import AddNewStudent from './AddNewStudent.svelte';
 	import EditStudent from './EditStudent.svelte';
+	import { Skeleton } from '$lib/components/ui/skeleton/index.ts';
 	
 	let isPageLoading = $state(true);
 	let students: Student[] = $state([]);
@@ -40,7 +41,7 @@
 </script>
 
 
-<div class="px-4 py-8 flex flex-col gap-3">
+<div class="container">
 	<span class="flex flex-row space-x-4 mb-4">
 		<a href="/sections"
 				class="font-bold button-outline h-fit">
@@ -51,30 +52,32 @@
       <h6>SECTION_ID {data.section_id}</h6>
     </span>
 	</span>
-	<Dialog.Root>
-		<Dialog.Trigger class="button-outline font-medium items-center">
-			(+) Add new student
-		</Dialog.Trigger>
-		<AddNewStudent section_id={data.section_id}/>
-	</Dialog.Root>
-	{#if isPageLoading}
-		<p>Loading...</p>
-	{:else}
-		{#each students as student}
-			<div class="card">
-				<span class="flex flex-row items-center justify-between">
-					<h3>{student.name}</h3>
-					<Dialog.Root>
-						<Dialog.Trigger class="button-secondary">
-							<MdiEdit class="size-4"/>
-						</Dialog.Trigger>
-						<EditStudent bind:section_id={data.section_id}
-													student_no={student.student_no}
-													bind:name={student.name}/>
-					</Dialog.Root>
-				</span>
-				<h5>{student.student_no}</h5>
-			</div>
-		{/each}
-	{/if}
+  <div class="space-y-2">
+    <Dialog.Root>
+      <Dialog.Trigger class="button-outline font-medium items-center w-full">
+        (+) Add new student
+      </Dialog.Trigger>
+      <AddNewStudent section_id={data.section_id}/>
+    </Dialog.Root>
+    {#if isPageLoading}
+      <Skeleton class="grayscale h-16"/>
+    {:else}
+      {#each students as student}
+      <div class="card">
+        <span class="flex flex-row items-center justify-between">
+          <h3>{student.name}</h3>
+          <Dialog.Root>
+            <Dialog.Trigger class="button-secondary">
+              <MdiEdit class="size-4"/>
+            </Dialog.Trigger>
+            <EditStudent bind:section_id={data.section_id}
+                          student_no={student.student_no}
+                          bind:name={student.name}/>
+          </Dialog.Root>
+        </span>
+        <h5>{student.student_no}</h5>
+      </div>
+      {/each}
+    {/if}
+  </div>
 </div>
