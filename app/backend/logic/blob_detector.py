1a455ed62059e52610ec6ca400408f8b281d4488
HoweZae <jdtomanan@up.edu.ph>
2026-03-17 21:08:16 +0800

    feat(backend): adjust params

--- app/backend/logic/blob_detector.py
+++ app/backend/logic/blob_detector.py
@@ -1,22 +1,32 @@
 from cv2.typing import MatLike
 import cv2
-from core.constants import *
+from core.constants import NORMAL_SIZE
 
 
 class BlobDetector:
     def __init__(self):
         params = cv2.SimpleBlobDetector_Params()
+        
         params.filterByColor = True
         params.blobColor = 0  # dark blobs
+        params.minThreshold = 10
+        params.maxThreshold = 180
+        params.thresholdStep = 5
+        
+        areaFactor = (NORMAL_SIZE/1000)**2
         params.filterByArea = True
-        params.minArea = NORMAL_SIZE*0.05
-        params.maxArea = NORMAL_SIZE*10.0
+        params.minArea = areaFactor * 50
+        params.maxArea = areaFactor * 6250
+        
         params.filterByCircularity = True
-        params.minCircularity = 0.55
+        params.minCircularity = 0.30
+        
         params.filterByConvexity = True
-        params.minConvexity = 0.80
+        params.minConvexity = 0.70
+        
         params.filterByInertia = True
         params.minInertiaRatio = 0.4
+        
         self._detector = cv2.SimpleBlobDetector_create(params)
 
     def detect(self, image_mat: MatLike) -> list[cv2.KeyPoint]:
