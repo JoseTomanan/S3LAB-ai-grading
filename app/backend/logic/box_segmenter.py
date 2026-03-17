e49e2011f268ac17f7d8e82d70e6f08bebd43412
HoweZae <jdtomanan@up.edu.ph>
2026-03-17 19:51:29 +0800

    BREAKING CHANGE refactor(backend): Segregate blob detector; only delegate with blob detection duty

--- app/backend/logic/box_segmenter.py
+++ app/backend/logic/box_segmenter.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2
+from itertools import combinations
 from cv2.typing import MatLike
+
 from core.constants import *
+
 from logic.document_scanner import DocumentScanner
 from logic.ai_interface import AIAnswerEvaluator
 from logic.blob_detector import BlobDetector
+from logic.utility import _get_robust_aspect_ratio, _is_valid_quad, _mapp
 
 
 
@@ -81,12 +85,61 @@ class BoxSegmenter(DocumentScanner):
                     "./TEMP/output/DEBUG_canny_regularize_dilate.jpg"
                     )
 
-        BLOB_DETECTOR = BlobDetector()
-        image_sections = BLOB_DETECTOR.detect_sections_via_anchors(image_original)
-
+        image_sections = self._detect_boxes_via_dots(image_original, debug=debug)
+        # TODO: sort accdg to upper left corner
         images_warped = [self._warp_from_original(i, image_original) for i in image_sections[:num_boxes]]
 
         return [self._encode_to_bytes(i) for i in images_warped]
+
+    def _detect_boxes_via_dots(self, image_mat: MatLike, debug: bool = False) -> list[MatLike]:
+        """
+        Detect rectangular sections in a preprocessed image using detected dark solid blobs (corners/anchors).
+
+        This function finds keypoints (anchor points) in the input image using a blob detector. It then considers 
+        all possible combinations of four keypoints, checks if they can form a valid quadrilateral based on area, 
+        aspect ratio, and skew angle, and collects those that pass these checks as valid sections. The returned
+        result is an ndarray where each element is a 4x2 array of corner points.
+
+        Args:
+            image_mat (MatLike): Input image for blob detection.
+
+        Returns:
+            list[MatLike]: An array of detected section corner arrays of shape (num_sections, 4, 2).
+        """
+        BLOB_DETECTOR = BlobDetector()
+
+        keypoints = BLOB_DETECTOR.detect(image_mat)
+        if len(keypoints) < 4:
+            print(f"INFO:\tOnly {len(keypoints)} blobs detected")
+            return []
+
+        pts = np.array([[kp.pt[0], kp.pt[1]] for kp in keypoints], dtype=np.float32)
+
+        if debug:
+            debug_img = image_mat.copy()
+            for p in pts:
+                debug_img = self._highlight_dot(debug_img, p)
+            self.save_image(self._encode_to_bytes(debug_img), "./TEMP/output/DEBUG_mark_dots.jpg")
+
+        if len(pts) == 4:
+            quad_candidates = [pts]
+        else:
+            quad_candidates = [pts[list(c)] for c in combinations(range(len(pts)), 4)]
+
+        image_good_sections = []
+        for i, c in enumerate(quad_candidates):
+            c
+            
+            if _is_valid_quad(c):
+                ordered = np.array(_mapp(c.flatten()), dtype=np.float32).reshape(4, 2)
+                image_good_sections.append(ordered)
+            else:
+                continue
+
+        if image_good_sections == []:
+            raise ValueError("Could not find any boxes.")
+        
+        return image_good_sections
 
     def beautify_scan(self, image_bytes: bytes) -> bytes:
         array = self._load_array(image_bytes)
@@ -95,6 +148,15 @@ class BoxSegmenter(DocumentScanner):
                             amount=1.3
                             )
         return self._unload_array(img)
+
+    #region Debugging functions
+    def _highlight_dot(self, image: MatLike, coordinate: tuple[int, int]) -> MatLike:
+        """FOR DEBUGGING; Highlight a single dot on the given image by drawing a green filled circle."""
+        debug_img = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR) if len(image.shape) == 2 else image.copy()
+        x, y = coordinate
+        cv2.circle(debug_img, (int(x), int(y)), radius=10, color=(0, 255, 0), thickness=-1)
+        return debug_img
+    #endregion
 
     #region Auxiliary functions
     def _regularize_forgivingly(self, image_mat: MatLike) -> list[MatLike]:
