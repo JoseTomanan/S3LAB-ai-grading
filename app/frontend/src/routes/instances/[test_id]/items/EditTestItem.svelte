f61063ee941bb12c17d1ff8d5dfdc695a9986b2d
HoweZae <jdtomanan@up.edu.ph>
2026-03-19 13:28:45 +0800

    fix(frontend): Fix wrong call, wrong expected return

--- app/frontend/src/routes/instances/[test_id]/items/EditTestItem.svelte
+++ app/frontend/src/routes/instances/[test_id]/items/EditTestItem.svelte
@@ -72,7 +72,8 @@
   async function deleteTestItem() {
     try {
       const response = await fetch(
-            `/api/test_items/${test_id}/${formTestItem.item_id}`,
+            // `/api/test_items/${test_id}/${formTestItem.item_id}`,
+            `/api/test_items/${test_id}/items/${formTestItem.item_id}`,
             {
               method: "DELETE",
               headers: {'Content-Type': 'application/json',},
@@ -80,7 +81,8 @@
             );
       
       switch (response.status) {
-        case 200:
+        // case 200:
+        case 204:
           alert("Delete success.");
           window.location.reload();
           break;
