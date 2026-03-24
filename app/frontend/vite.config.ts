ed7bdc36e7997f9ee99eaa674b6930c1b16db2d3
JoseTomanan <jdtomanan@up.edu.ph>
2026-03-24 17:10:40 +0800

    chore(Frontend): Reenable HTTPS

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -18,7 +18,7 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
-    // basicSsl(),
+    basicSsl(),
 		Icons({
 			compiler: 'svelte',
 		}),
