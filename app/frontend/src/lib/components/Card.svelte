3bbc4370e91fd64a1ef0499759cf81ba01b81e43
HoweZae <jdtomanan@up.edu.ph>
2026-03-30 00:11:57 +0800

    style: make border darker

--- app/frontend/src/lib/components/Card.svelte
+++ app/frontend/src/lib/components/Card.svelte
@@ -19,7 +19,7 @@
 
 <svelte:element this={tag} 
         {...(href ? { href } : {})}
-        class="bg-card text-card-foreground rounded-xs border border-border/20 px-3 py-1.5 {className}" 
+        class="bg-card text-card-foreground rounded-xs border border-border/35 px-3 py-1.5 {className}" 
         {...rest}>
   {@render children()}
 </svelte:element>
