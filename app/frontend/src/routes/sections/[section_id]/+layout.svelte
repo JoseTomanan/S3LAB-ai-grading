2cf39a501872e24578a5c8e267ac7acdf37ac2a5
HoweZae <jdtomanan@up.edu.ph>
2026-03-30 22:39:24 +0800

    feat(Frontend): Implement section layout loading and display section name

--- app/frontend/src/routes/sections/[section_id]/+layout.svelte
+++ app/frontend/src/routes/sections/[section_id]/+layout.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
   const { data, children } = $props();
   import IconBack from "~icons/mdi/arrow-left";
+
+  const sectionId = Number(data.section_id);
+  const sectionName = data.sections.find(
+    (section) => section.section_id === sectionId
+  )!.section_name;
 </script>
 
 
 <nav class="bg-sidebar text-sidebar-foreground
             shadow shadow-sidebar-border
             w-full flex flex-row justify-between p-4 pt-6">
-  <span class="*:leading-5">
-    <h1 class="align-left italic">Class List</h1>
-    <h6>SECTION_ID {data.section_id}</h6>
+  <span class="*:leading-6">
+    <h1 class="align-left">
+      {sectionName}
+    </h1>
+    <h5>Class list</h5>
   </span>
   <a href="/sections">
     <IconBack class="size-8"/>
