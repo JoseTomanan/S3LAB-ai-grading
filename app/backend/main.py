ee2110613db8725dfe86d8f6b8f085835db95c17
HoweZae <jdtomanan@up.edu.ph>
2026-04-04 23:05:39 +0800

    fix: Missing import

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
