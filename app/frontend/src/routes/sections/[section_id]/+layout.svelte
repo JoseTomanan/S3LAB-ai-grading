8e0fa515fe12329ec48ec6b0c0a4cbed26dd202c
HoweZae <jdtomanan@up.edu.ph>
2026-04-05 14:55:05 +0800

    style: Add separator; Add skeleton for `navigating.to`

--- app/frontend/src/routes/sections/[section_id]/+layout.svelte
+++ app/frontend/src/routes/sections/[section_id]/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import { navigating } from '$app/state';
   const { data, children } = $props();
+  
   import { Button } from '$lib/components/CustomButton/index.ts';
+  import { Separator } from '$lib/components/ui/separator';
+  import { Skeleton } from '$lib/components/ui/skeleton';
   import IconBack from "~icons/mdi/chevron-left";
   import IconSectionList from '~icons/mdi/format-list-bulleted';
 
@@ -14,19 +18,29 @@
 
 <nav class="bg-sidebar text-sidebar-foreground
             shadow shadow-sidebar-border
-            w-full flex flex-row items-center justify-between p-4 pt-6">
-  <Button variant="floating" href="/sections">
-    <IconBack class="size-8"/>
-  </Button>
-  <span class="*:leading-5 *:text-center">
-    <h2>{sectionName}</h2>
-  </span>
-  <Button variant="floating" href="/sections">
-    <IconSectionList class="size-7 m-0.5"/>
-  </Button>
+            w-full flex flex-col gap-y-2.5 p-4 pt-6">
+  <div class="w-full inline-flex items-center justify-between">
+    <Button variant="floating" href="/sections">
+      <IconBack class="size-8"/>
+    </Button>
+    <span class="*:leading-5 *:text-center">
+      <h2>{sectionName}</h2>
+    </span>
+    <Button variant="floating" href="/sections">
+      <IconSectionList class="size-7 m-0.5"/>
+    </Button>
+  </div>
+
+  <Separator/>
 </nav>
 
 <span class="-my-4"></span>
 
-{@render children()}
+{#if navigating.to}
+  <div class="container">
+    <Skeleton class="grayscale-50 w-full h-64 rounded-none"/>
+  </div>
 
+{:else}
+  {@render children()}
+{/if}
